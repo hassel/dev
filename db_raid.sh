d2e72455a7dbf18508e19b0abc109c6e9544f7c2
Carl Hasselström <carhas@hassel.stho.kambi.com>
2014-07-09 14:43:16 +0200

    derp

---
+++ db_raid.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -u
+# derp
+
+ECHO=/bin/echo
+PARTED=/sbin/parted
+MKDIR=/bin/mkdir
+MOUNT=/bin/mount
+MKFS=/sbin/mkfs.xfs
+ID=/usr/bin/id
+CHOWN=/bin/chown
+USER="postgres"
+
+function _get_disk_type () {
+        if [ -b "/dev/sdb" ]; then
+          device="/dev/sdb"
+        elif [ -b "/dev/cciss/c0d1p1" ]; then
+          device="/dev/cciss/c0d1p1"
+        elfi
+          $ECHO "Error, devivce not found"
+          exit 1
+        fi
+} 
+
+function _mkfs_and_dir (){
+  if [ -x /sbin/mkfs.xfs ]; then
+          $ID -u $USER
+    if [ "$?" == "0" ]; then
+          DIR="/var/lib/postgresql"
+       	  PART="$(echo $device)1"
+          $MKDIR $DIR 
+          $CHOWN -R $USER:$USER $DIR
+          $PARTED -s -a optimal $device mklabel gpt -- mkpart logical xfs 1 -1
+	        $MKFS -f $PART
+          $ECHO "$PART $DIR   xfs     noatime,defaults        0       0" >> /etc/fstab
+    else
+          $ECHO "User postgres doesn't exist, please kerberise"
+          exit 1
+    fi
+  else
+          $ECHO "xfs not avalible"
+          $ECHO "Please : apt-get install xfsprogs"
+          exit 1
+  fi
+
+}
+
+function _mount () {
+       	$MOUNT -a
+}
+
+	
+function _main () {
+        _get_disk_type
+        _mkfs_and_dir
+        _mount
+}
+
+_main
---
+++ dns_test.sh
@@ -0,0 +1,132 @@
+#!/bin/bash -u
+# 
+# omg lol
+# please help me to find out if we fucked up internaly externaly or both !!!1
+# Hassel - 2013
+#
+DIG=/usr/bin/dig
+NS_INT_ROOT="ns.sth.kambi.com"
+REF="217.75.96.11 8.8.8.8"
+
+_get_int_ns(){
+        $DIG ns services.kambi.com +short
+}
+_get_ext_ns(){
+        domain=$(echo $@ | awk -F . '{ print $(NF-1) "." $NF }')
+        $DIG ns $domain +short
+}
+_ext_recursion(){
+        $DIG $@ +short | sort
+}
+_kambi_authoritative(){
+        $DIG -p53 +short @$NS $@ | sort
+}
+_root_int(){
+        $DIG -p53 +short @$NS_INT_ROOT $@ | sort
+}
+_kambi_recursive(){
+        $DIG -p54 +short @$NS $@ | sort
+}
+_compare_external_internal(){
+        echo -e "\E[32m\E[1m*  \E[0m Record/domain doesn't seem to be owned by kambi                                             \E[34m\E[1m[\E[33m !! \E[34m]\E[0m" 
+        echo -e "\E[32m\E[1m*  \E[0m Fetching in ns from interwebz"
+        EXT_NS=$(_get_ext_ns $@)
+        if [ -z "$EXT_NS" ]; then
+                echo -e "\E[32m\E[1m*  \E[0m Record/domain doesn't seem to owned by anyone ?                                             \E[34m\E[1m[\E[33m !! \E[34m]\E[0m"
+                exit 1
+        fi
+        INT_NS=$(_get_int_ns)
+        echo -e "\E[32m\E[1m*  \E[0m Done" 
+        for NS in $INT_NS; do
+                for QUERY in $EXT_NS; do
+                        if [ "$(_kambi_recursive $@)" == "$(_ext_recursion $@ @$QUERY)" ];
+                                then 
+                                        echo -e "\E[32m\E[1m*  \E[0m Recrusion successfull for $@ (for $NS via $QUERY)    \E[34m\E[1m[\E[32m ok \E[34m]\E[0m"
+                                fi
+                done
+        done
+}
+_compare_internal_external(){
+        EXT_NS=$(echo $REF)
+        INT_NS=$(_get_int_ns)
+        for NS in $INT_NS; do
+                for QUERY in $EXT_NS; do
+                        if [ "$(_kambi_recursive $@)" == "$(_ext_recursion $@ @$QUERY)" ];
+                                then 
+                                        echo -e "\E[32m\E[1m*  \E[0m Recrusion successfull for $@ (via $QUERY)    \E[34m\E[1m[\E[32m ok \E[34m]\E[0m"
+                                else
+                                        if [ -z "$(_ext_recursion $@ @$QUERY)" ];
+                                        then
+                                                echo -e "\E[32m\E[1m*  \E[0m Record NOT visible on the interewbz   \E[34m\E[1m[\E[33m !! \E[34m]\E[0m"
+                                                break
+                                        else
+                                                echo -e "\E[32m\E[1m*  \E[0m Diff between internal & external answer."
+                                                echo -e "\E[32m\E[1m*  \E[0m External "$(_ext_recursion $@ @$QUERY)"  \E[34m\E[1m[\E[33m !! \E[34m]\E[0m"
+                                                echo -e "\E[32m\E[1m*  \E[0m Internal "$(_kambi_recursive $@)"        \E[34m\E[1m[\E[33m !! \E[34m]\E[0m"
+                                        fi
+
+                                fi
+                done
+        done
+}
+
+
+_compare_internal_unbound_nsd(){
+                echo -e "\E[32m\E[1m*  \E[0m Record seems to owned by kambi                                                \E[34m\E[1m[\E[32m ok \E[34m]\E[0m"
+                INT_NS=$(_get_int_ns)
+                for NS in $INT_NS;
+                do
+                        if [ "$(_kambi_recursive $@)" == "$(_root_int $@)"  ]; then
+                                echo -e "\E[32m\E[1m*  \E[0m Unbound at $NS in sync with $NS_INT_ROOT                   \E[34m\E[1m[\E[32m ok \E[34m]\E[0m"
+                        else
+                                echo -e "\E[32m\E[1m*  \E[0m Unbound at $NS NOT in sync with $NS_INT_ROOT               \E[34m\E[1m[\E[33m !! \E[34m]\E[0m" 
+                        fi
+                        if [ "$(_kambi_authoritative $@)" == "$(_root_int $@)" ]; then
+                                echo -e "\E[32m\E[1m*  \E[0m NSD at $NS in sync with $NS_INT_ROOT                       \E[34m\E[1m[\E[32m ok \E[34m]\E[0m"
+                        else
+                                echo -e "\E[32m\E[1m*  \E[0m NSD at $NS NOT in sync with $NS_INT_ROOT                   \E[34m\E[1m[\E[33m !! \E[34m]\E[0m"
+                        fi
+                done
+}
+_full(){
+        if  [ -z "$(_root_int $@)" ];
+        then
+                _compare_external_internal $@
+        else
+                _compare_internal_unbound_nsd $@
+                _compare_internal_external $@
+fi
+}
+
+_get_opts(){
+                while getopts :i:e:a:h opt; do
+                        case $opt in
+                                a)
+                                        _full $2
+                                        ;;
+                                i)
+                                        _compare_internal_unbound_nsd $2
+                                        ;;
+                                e)
+                                        _compare_internal_external $2
+                                        ;;
+                                h)
+                                        echo -e "\E[32m\E[1m*  \E[0m  Usage:"
+                                        echo -e "\E[32m\E[1m*  \E[0m  $(basename $0) -i record.domain (internal check)" 
+                                        echo -e "\E[32m\E[1m*  \E[0m  $(basename $0) -e record.domain (external check)"
+                                        echo -e "\E[32m\E[1m*  \E[0m  $(basename $0) -a record.domain (check both)"
+                                        echo -e "\E[32m\E[1m*  \E[0m  $(basename $0) -h (this message)"
+                                        echo -e "\E[32m\E[1m*  \E[0m " 
+                                        ;;
+                                *)
+                                        $$ -h
+                                        ;;
+                        esac
+                done
+ }
+
+if [ "$#" -lt "1" ]; then
+        _get_opts -h
+  else
+        _get_opts $@
+fi
